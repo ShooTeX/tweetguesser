7b541ffec91e987be1cb5a1fd1a75125e3f45c89
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2023-01-29 23:54:52 +0100

    twitter token

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -6,6 +6,7 @@ import { z } from "zod";
  * This way you can ensure the app isn't built with invalid env vars.
  */
 export const serverSchema = z.object({
+  TWITTER_TOKEN: z.string().length(114),
   DATABASE_URL: z.string().url(),
   NODE_ENV: z.enum(["development", "test", "production"]),
 });
@@ -16,6 +17,7 @@ export const serverSchema = z.object({
  * @type {{ [k in keyof z.input<typeof serverSchema>]: string | undefined }}
  */
 export const serverEnv = {
+  TWITTER_TOKEN: process.env.TWITTER_TOKEN,
   DATABASE_URL: process.env.DATABASE_URL,
   NODE_ENV: process.env.NODE_ENV,
 };
