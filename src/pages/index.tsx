ec36cdcfa0a499bfe2b538dcbf8d28374f3a975e
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2023-02-01 22:25:29 +0100

    improve round init?

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { RESET } from "jotai/utils";
 import { type NextPage } from "next";
 import Head from "next/head";
 import { useState } from "react";
-import { currentRoundAtom } from "../atoms/game";
+import { currentRoundAtom, roundsAtom } from "../atoms/game";
 import { GuessInput } from "../components/GuessInputs";
 import { Stats } from "../components/Stats";
 import { Timer } from "../components/Timer";
@@ -11,29 +11,35 @@ import { Tweet, TweetLoading } from "../components/Tweet";
 
 import { api } from "../utils/api";
 import type { RouterOutputs } from "../utils/api";
+import { useImmerAtom } from "jotai-immer";
 
 const Home: NextPage = () => {
   const [currentRound, setCurrentRound] = useAtom(currentRoundAtom);
+  const [rounds, setRounds] = useImmerAtom(roundsAtom);
   const [currentTweet, setCurrentTweet] = useState<
     RouterOutputs["twitter"]["getNextTweet"] | undefined
   >();
 
-  const { data } = api.twitter.getNextTweet.useQuery(undefined, {
+  const { data, refetch } = api.twitter.getNextTweet.useQuery(undefined, {
     refetchOnWindowFocus: false,
     refetchOnMount: false,
   });
 
-  const initNewRound = (possibleAnswers: string[]) => {
+  const initNewRound = async () => {
+    setRounds((draft) => {
+      draft.push(currentRound);
+    });
     setCurrentRound(RESET);
-    setCurrentRound((round) => ({
-      ...round,
-      possibleAnswers,
-    }));
+    await refetch();
   };
 
   if (data && data !== currentTweet) {
     setCurrentTweet(data);
-    initNewRound([...data.possibleNames]);
+    setCurrentRound((round) => ({
+      ...round,
+      status: "playing",
+      possibleAnswers: [...data.possibleNames],
+    }));
   }
 
   return (
