c8248a0e89be8ab36eb684687ee3bd1f1c40fb2b
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2023-02-11 22:16:21 +0100

    comment out unnused component

--- src/components/Lives.tsx
+++ src/components/Lives.tsx
@@ -1,22 +1,22 @@
-import { useAtom } from "jotai";
-import { FaHeart, FaHeartBroken } from "react-icons/fa";
-import { gameConfigAtom } from "../atoms/game";
+// import { useAtom } from "jotai";
+// import { FaHeart, FaHeartBroken } from "react-icons/fa";
+// import { gameConfigAtom } from "../atoms/game";
 
 type LivesProps = {
   tries: number;
 };
 
-export const Lives = ({ tries }: LivesProps) => {
-  const [config] = useAtom(gameConfigAtom);
+export const Lives = ({}: LivesProps) => {
+  // const [config] = useAtom(gameConfigAtom);
 
   return (
     <div className="flex space-x-1 text-lg">
-      {[...Array<undefined>(config.maxLives - tries)].map((_, i) => (
-        <FaHeart className="text-success" key={`heart-${i}`} />
-      ))}
-      {[...Array<undefined>(tries)].map((_, i) => (
-        <FaHeartBroken className="text-error" key={`fail-${i}`} />
-      ))}
+      {/* {[...Array<undefined>(config.maxLives - tries)].map((_, i) => ( */}
+      {/*   <FaHeart className="text-success" key={`heart-${i}`} /> */}
+      {/* ))} */}
+      {/* {[...Array<undefined>(tries)].map((_, i) => ( */}
+      {/*   <FaHeartBroken className="text-error" key={`fail-${i}`} /> */}
+      {/* ))} */}
     </div>
   );
 };
