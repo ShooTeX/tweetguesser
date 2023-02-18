bdb9ec4fa60f336c47d75655b30c1b97f5c4a751
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2023-02-18 04:26:11 +0100

    fix og image

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,13 +19,24 @@ const MyApp: AppType = ({ Component, pageProps }) => {
         <meta property="og:url" content="https://www.tweetguesser.com" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="TWEETGUESSER" />
-        <meta property="og:image" content="/images/og-image.jpeg" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta property="twitter:domain" content="tweetguesser.com" />
         <meta property="twitter:url" content="https://www.tweetguesser.com" />
         <meta name="twitter:title" content="TWEETGUESSER" />
         <meta name="twitter:description" content="Test your twitter skills!" />
-        <meta name="twitter:image" content="/images/og-image.jpeg" />
+
+        {process.env.VERCEL_URL && (
+          <>
+            <meta
+              name="twitter:image"
+              content={`${process.env.VERCEL_URL}/images/og-image.jpeg`}
+            />
+            <meta
+              property="og:image"
+              content={`${process.env.VERCEL_URL}/images/og-image.jpeg`}
+            />
+          </>
+        )}
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="icon" href="/favicon.ico" />
