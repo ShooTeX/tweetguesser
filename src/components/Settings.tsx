546efe295be509142321c823f9d54ee1d21e0ef0
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2023-02-19 00:53:53 +0100

    settings component

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,10 +1,41 @@
+import { useAtom } from "jotai";
+import { useForm } from "react-hook-form";
+import { gameConfigAtom, type GameConfig } from "../atoms/game";
+import { equals } from "remeda";
+
 export const Settings = () => {
+  const [config, setConfig] = useAtom(gameConfigAtom);
+  const {
+    register,
+    watch,
+    reset,
+    formState: { defaultValues, touchedFields },
+  } = useForm<GameConfig>({
+    defaultValues: config,
+  });
+
+  const isTouched = !!Object.keys(touchedFields).length;
+
+  const formData = watch();
+
+  if (!equals(config, defaultValues) && !isTouched) {
+    reset(config);
+  }
+
+  if (!equals(config, formData) && isTouched) {
+    setConfig(formData);
+  }
+
   return (
-    <div>
+    <form>
       <div className="form-control w-full max-w-[13rem]">
         <label className="label cursor-pointer">
           <span className="label-text">Endless Mode</span>
-          <input type="checkbox" className="toggle-primary toggle" />
+          <input
+            type="checkbox"
+            className="toggle-primary toggle"
+            {...register("endless")}
+          />
         </label>
       </div>
       <div className="form-control">
@@ -14,30 +45,34 @@ export const Settings = () => {
         <div className="btn-group">
           <input
             type="radio"
-            name="options"
+            value={"today"}
             data-title="Today"
             className="btn"
+            {...register("startTime")}
           />
           <input
             type="radio"
-            name="options"
+            value={"1_month_ago"}
             data-title="1 month ago"
             className="btn"
+            {...register("startTime")}
           />
           <input
             type="radio"
-            name="options"
+            value={"1_year_ago"}
             data-title="1 year ago"
             className="btn"
+            {...register("startTime")}
           />
           <input
             type="radio"
-            name="options"
+            value={"3_years_ago"}
             data-title="3 years ago"
             className="btn"
+            {...register("startTime")}
           />
         </div>
       </div>
-    </div>
+    </form>
   );
 };
