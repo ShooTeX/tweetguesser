d2a8edf867d3a651ab7e584032c3292565785db3
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2023-03-06 19:44:29 +0100

    websockets

--- apps/fastify-trpc/src/index.ts
+++ apps/fastify-trpc/src/index.ts
@@ -4,15 +4,19 @@ import { fastifyTRPCPlugin } from "@trpc/server/adapters/fastify";
 import fastify from "fastify";
 import { appRouter } from "./router";
 import { createContext } from "./trpc";
+import ws from "@fastify/websocket";
 
 const server = fastify({
   maxParamLength: 5000,
   logger: process.env.NODE_ENV !== "production",
 });
 
+void server.register(ws);
+
 void server.register(fastifyTRPCPlugin, {
   prefix: "/trpc",
   trpcOptions: { router: appRouter, createContext },
+  useWSS: true,
 });
 
 try {
