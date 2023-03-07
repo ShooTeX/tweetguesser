3b1b580d12955c5be24db447a37ee8675a1f2ee7
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2023-03-07 19:43:59 +0100

    updates

--- apps/fastify-trpc/src/index.ts
+++ apps/fastify-trpc/src/index.ts
@@ -5,6 +5,8 @@ import fastify from "fastify";
 import { appRouter } from "./router";
 import { createContext } from "./trpc";
 import ws from "@fastify/websocket";
+import getPort from "get-port";
+import { PORT } from "./environment";
 
 const server = fastify({
   maxParamLength: 5000,
@@ -20,7 +22,9 @@ void server.register(fastifyTRPCPlugin, {
 });
 
 try {
-  await server.listen({ port: 3000 });
+  await server.listen({
+    port: await getPort({ port: [PORT, 3000, 3001, 3002] }),
+  });
   console.info("Server started");
 } catch (error) {
   server.log.error(error);
