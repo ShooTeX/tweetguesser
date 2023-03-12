afadfec9b654c1fa27c46373583999b0d4c6d20b
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2023-03-12 23:05:27 +0100

    add layout to reusable animation

--- apps/web/src/components/handle-list.tsx
+++ apps/web/src/components/handle-list.tsx
@@ -5,6 +5,7 @@ import { Bomb } from "lucide-react";
 import { usernamesAtom } from "../atoms/game";
 
 const badgeAnimations: HTMLMotionProps<"span"> = {
+  layout: true,
   initial: {
     x: 20,
     opacity: 0,
@@ -36,7 +37,6 @@ export const HandleList = () => {
       <AnimatePresence mode="popLayout">
         {usernames.map((username) => (
           <motion.span
-            layout
             key={username}
             className="badge cursor-pointer transition-none"
             onClick={() => handleClick(username)}
@@ -47,7 +47,6 @@ export const HandleList = () => {
         ))}
         {usernames.length > 1 && (
           <motion.span
-            layout
             key="remove-all"
             onClick={() => updateUsernames([])}
             className="badge badge-outline badge-error cursor-pointer transition-none"
