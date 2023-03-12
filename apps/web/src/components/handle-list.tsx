0989197bcd86c29db16a73dcec058cfa10aa6677
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2023-03-12 23:58:03 +0100

    add invalid usernames

--- apps/web/src/components/handle-list.tsx
+++ apps/web/src/components/handle-list.tsx
@@ -1,8 +1,8 @@
 import type { HTMLMotionProps } from "framer-motion";
 import { AnimatePresence, motion } from "framer-motion";
-import { useAtom } from "jotai";
+import { useAtom, useAtomValue } from "jotai";
 import { Bomb } from "lucide-react";
-import { usernamesAtom } from "../atoms/game";
+import { invalidUsernamesAtom, usernamesAtom } from "../atoms/game";
 
 const badgeAnimations: HTMLMotionProps<"span"> = {
   layout: true,
@@ -27,15 +27,33 @@ const badgeAnimations: HTMLMotionProps<"span"> = {
 
 export const HandleList = () => {
   const [usernames, updateUsernames] = useAtom(usernamesAtom);
+  const cachedInvalidUsernames = useAtomValue(invalidUsernamesAtom);
+  const invalidUsernames = usernames.filter((username) =>
+    cachedInvalidUsernames.includes(username)
+  );
+  const validUsernames = usernames.filter(
+    (username) => !invalidUsernames.includes(username)
+  );
   const handleClick = (input: string) => {
     updateUsernames((usernames) =>
       usernames.filter((username) => username !== input)
     );
   };
+
   return (
     <div className="flex flex-row flex-wrap gap-1">
       <AnimatePresence mode="popLayout">
-        {usernames.map((username) => (
+        {invalidUsernames.map((username) => (
+          <motion.span
+            key={username}
+            className="badge badge-error cursor-pointer transition-none"
+            onClick={() => handleClick(username)}
+            {...badgeAnimations}
+          >
+            {username}
+          </motion.span>
+        ))}
+        {validUsernames.map((username) => (
           <motion.span
             key={username}
             className="badge cursor-pointer transition-none"
