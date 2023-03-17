82ae0d3d408f5e9f814c4a51978e242b1c63ca36
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2023-03-17 14:19:16 +0100

    add option to hide urls

--- apps/web/src/components/settings.tsx
+++ apps/web/src/components/settings.tsx
@@ -1,6 +1,7 @@
 import { useAtom } from "jotai";
 import { gameConfigAtom, type EndTime, endTimeSchema } from "../atoms/game";
 import { type ChangeEventHandler } from "react";
+import { EyeOff, History, Infinity } from "lucide-react";
 
 type SettingsProperties = {
   onEndTimeChange?: () => void;
@@ -33,11 +34,23 @@ export const Settings = ({
     onEndlessChange && onEndlessChange();
   };
 
+  const handleHideUrlsChange: ChangeEventHandler<HTMLInputElement> = (
+    event
+  ) => {
+    setConfig((config) => ({
+      ...config,
+      hideUrls: event.target.checked,
+    }));
+  };
+
   return (
     <form className="space-y-4">
       <div className="form-control">
         <label className="label">
-          <span className="label-text">Tweets starting from</span>
+          <span className="label-text flex items-center">
+            <History className="mr-1 h-4 w-4" />
+            Tweets starting from
+          </span>
         </label>
         <select
           className="select select-bordered w-full"
@@ -55,9 +68,34 @@ export const Settings = ({
           ))}
         </select>
       </div>
-      <div className="form-control w-full max-w-[13rem]">
+      <div className="form-control w-full">
         <label className="label cursor-pointer">
-          <span className="label-text">Endless Mode</span>
+          <span className="label-text flex flex-col">
+            <span className="flex items-center">
+              <EyeOff className="mr-1 h-4 w-4" />
+              Hide URLs
+            </span>
+            <span className="label-text-alt opacity-70">
+              Don&apos;t show urls in tweets
+            </span>
+          </span>
+          <input
+            type="checkbox"
+            className="toggle-primary toggle"
+            checked={config.hideUrls}
+            onChange={handleHideUrlsChange}
+          />
+        </label>
+      </div>
+      <div className="form-control w-full">
+        <label className="label cursor-pointer">
+          <span className="label-text flex flex-col">
+            <span className="flex items-center justify-center">
+              <Infinity className="mr-1 h-4 w-4" />
+              Endless Mode
+            </span>
+            <span className="label-text-alt opacity-70">Play forever...</span>
+          </span>
           <input
             type="checkbox"
             className="toggle-primary toggle"
